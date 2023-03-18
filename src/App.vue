1b260a200674f39fb49d0e1f20bb6cc1842a71db
AnastasiyaV <anastasiya.vygovskaya@gmail.com>
2023-03-18 12:59:18 +0300

    deleted files

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,64 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div class="app">
+        <post-form @create="createPost"/>
+        <post-list :posts="posts" />
+        
+    </div>
 </template>
 
+<script>
+import PostForm from "@/components/PostForm.vue"
+import PostList from "@/components/PostList.vue";
+
+export default {
+    components: {
+        PostList, PostForm
+    },
+    data() {
+        return {
+            posts: [
+                {id: 1, title: 'JavaScript', body: 'Описание поста'},
+                {id: 2, title: 'JavaScript2', body: 'Описание поста 2'},
+                {id: 3, title: 'JavaScript3', body: 'Описание поста 3'},
+                {id: 4, title: 'JavaScript4', body: 'Описание поста 4'},
+            ],
+        }
+    },
+    methods: {
+        createPost(post) {
+            console.log(post);
+            this.posts.push(post);
+
+            // if (this.title == '' || this.body == '')
+            // return;
+
+            // const newPost = {
+            //     id: Date.now,
+            //     title: this.title,
+            //     body: this.body,
+            // }
+            // this.posts.push(newPost);
+            // this.title = '';
+            // this.body = '';
+        },
+        inputTitle(event) {
+            this.title = event.target.value;
+        }
+    }
+}
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.app {
+    padding: 20px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
