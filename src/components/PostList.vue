04a2914f696fe0b8b153d464a566252582b90695
AnastasiyaV <anastasiya.vygovskaya@gmail.com>
2023-03-18 12:55:32 +0300

    add posts

---
+++ src/components/PostList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h3>Список пользователей</h3>
+        <post-item v-for="post in posts" :post="post"/>
+    </div>
+</template>
+
+<script>
+import PostItem from '@/components/PostItem.vue';
+
+    export default {
+    props: {
+        posts: {
+            type: Array,
+            required: true
+        }
+    },
+    components: { PostItem }
+}
+</script>
+
+<style scoped>
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+</style>
