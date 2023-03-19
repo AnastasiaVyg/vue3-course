e04562c43b32945644b0da46fbfbcfe4d3aba7ba
AnastasiyaV <anastasiya.vygovskaya@gmail.com>
2023-03-19 22:09:53 +0300

    get posts with button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="app">
         <h1>Страница с постами</h1>
+        <my-button @click="fetchPosts">Получить посты</my-button>
         <my-button @click="showDialog" style="margin: 15px 0;">Создать пользователя</my-button>
         <my-dialog v-model:show="dialogVisible">
             <post-form @create="createPost"/>
@@ -14,19 +15,14 @@
 <script>
 import PostForm from "@/components/PostForm.vue"
 import PostList from "@/components/PostList.vue";
-
+import axios from 'axios';
 export default {
     components: {
         PostList, PostForm
     },
     data() {
         return {
-            posts: [
-                {id: 1, title: 'JavaScript', body: 'Описание поста'},
-                {id: 2, title: 'JavaScript2', body: 'Описание поста 2'},
-                {id: 3, title: 'JavaScript3', body: 'Описание поста 3'},
-                {id: 4, title: 'JavaScript4', body: 'Описание поста 4'},
-            ],
+            posts: [],
             dialogVisible: false,
         }
     },
@@ -43,6 +39,15 @@ export default {
         },
         showDialog() {
             this.dialogVisible = true;
+        },
+        async fetchPosts() {
+            try {
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+                this.posts = response.data
+                console.log(response)
+            } catch (error) {
+                alert('Ошибка')
+            }
         }
     }
 }
